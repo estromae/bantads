6e8ebcf984ccb81a185e0762f08d84d21e1efe58
Rafael <rafaelsoares.p.f@gmail.com>
2024-04-15 01:43:25 -0300

    campos de filtrar por cpf e por nome agora estão funcionando, página concluida, submit do formulário corrigido, filtra primeiro por cpf e depois por nome.Quando filtrado, os clientes que não corresponde a busca não aparecem

--- src/app/gerente/consultar-todos-os-clientes-gerente/consultar-todos-os-clientes-gerente.component.ts
+++ src/app/gerente/consultar-todos-os-clientes-gerente/consultar-todos-os-clientes-gerente.component.ts
@@ -16,23 +16,37 @@ export class ConsultarTodosOsClientesGerenteComponent {
 
   constructor() {
     this.ClientForms = new FormGroup({
-      nome: new FormControl('', Validators.required),
-      cpf: new FormControl('', Validators.required)
+      nome: new FormControl('', [Validators.required]),
+      cpf: new FormControl('', [Validators.required])
   });
   }
-  submit(){
-    return this.clientes.filter(cliente => cliente.cpf.includes(this.cpfPesquisado))
+
+  submit(){ 
+    var cpfPesquisado = this.ClientForms.controls['cpf'].value;
+    var nomePesquisado = this.ClientForms.controls['nome'].value;
+    console.log(cpfPesquisado)
+    let clienteEncontrado = this.clientes.find(cliente => cliente.cpf === cpfPesquisado);
+    console.log(clienteEncontrado)
+    if (!clienteEncontrado){
+       clienteEncontrado = this.clientes.find(cliente => cliente.nome === nomePesquisado);
+    } 
+    if (clienteEncontrado){
+      this.clientesPesquisados = [clienteEncontrado];
+    }
+    else{ 
+      this.clientesPesquisados = this.clientes
+    }
   }
 
-  cpfPesquisado : string = '';
-
-  //filtrarClientes(): any[] {
-  //  return this.clientes.filter(cliente => cliente.cpf.includes(this.cpfPesquisado));
-  //}
+  clientesPesquisados = [
+    { id: 1, cpf: '111.111.111-11', nome: 'Nome1', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+    { id: 2, cpf: '222.222.222-22', nome: 'Nome2', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+    { id: 3, cpf: '333.333.333-33', nome: 'Nome3', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+  ]
 
   clientes = [
-    { id: 1, cpf: '111.111.111-11', nome: 'Nome', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
-    { id: 2, cpf: '222.222.222-22', nome: 'Nome', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
-    { id: 3, cpf: '333.333.333-33', nome: 'Nome', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+    { id: 1, cpf: '111.111.111-11', nome: 'Nome1', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+    { id: 2, cpf: '222.222.222-22', nome: 'Nome2', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
+    { id: 3, cpf: '333.333.333-33', nome: 'Nome3', saldo: 'R$0.00', cidade: 'Cidade', estado: 'Estado' },
   ];
 }
