1dbeabd0d066804e2acf56c7b0a790f9da65d9b2
Rafael Lozano da Luz <rafaellozanopro@outlook.com>
2024-04-20 01:22:02 -0300

    Atualizando angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,16 @@
-import { Routes } from '@angular/router';
-import { TelaInicialComponent } from './Cliente/tela-inicial/tela-inicial.component';
+import { Routes } from '@angular/router';  
 import { TelaInicialGerenteComponent } from './gerente/tela-inicial-gerente/tela-inicial-gerente.component';
 import { AutocadastroComponent } from './autenticacao/autocadastro/autocadastro.component';
 import { LoginComponent } from './autenticacao/login/login.component';
 import { AlterarPerfilComponent } from './cliente/alterar-perfil/alterar-perfil.component';
 import { ConsultarClienteComponent } from './gerente/consultar-cliente/consultar-cliente.component';
 import { ConsultarTodosOsClientesGerenteComponent } from './gerente/consultar-todos-os-clientes-gerente/consultar-todos-os-clientes-gerente.component';
-
 import { MelhoresClientesComponent} from './gerente/melhores-clientes/melhores-clientes.component'
-export const routes: Routes = [
+export const routes: Routes = [ 
   {
-    path: 'telainicial',
-    component: TelaInicialComponent,
-  },
+    path: '',
+    component: LoginComponent,
+  }, 
   {
     path: 'autocadastro',
     component: AutocadastroComponent,
@@ -22,11 +20,23 @@ export const routes: Routes = [
     component: LoginComponent,
   },
   {
-    path: 'cliente',
-    component: TelaInicialComponent,
-  },
-  {
     path: "alterar-perfil",
     component: AlterarPerfilComponent
+  },
+  {
+    path: 'gerente/tela-inicial',
+    component: TelaInicialGerenteComponent
+  },
+  {
+    path: 'gerente/melhores-clientes',
+    component: MelhoresClientesComponent
+  },
+  {
+    path: 'gerente/consultar-cliente',
+    component: ConsultarClienteComponent
+  },
+  {
+    path: 'gerente/consultar-todos-clientes',
+    component: ConsultarTodosOsClientesGerenteComponent
   }
 ];
