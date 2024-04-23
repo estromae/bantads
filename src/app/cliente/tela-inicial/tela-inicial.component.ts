324b63634a895bc9d7645e7ac21ed2cac02e9cd9
Braian Viana <42611703+brksaian@users.noreply.github.com>
2024-04-23 12:38:22 -0300

    Update tela-inicial.component.ts

--- src/app/cliente/tela-inicial/tela-inicial.component.ts
+++ src/app/cliente/tela-inicial/tela-inicial.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @Component({
-  selector: 'app-tela-inicial',
+  selector: "app-tela-inicial",
   standalone: true,
-  imports: [DatePipe],
-  templateUrl: './tela-inicial.component.html',
-  styleUrl: './tela-inicial.component.css',
+  imports: [TransactionComponent],
+  templateUrl: "./tela-inicial.component.html",
+  styleUrl: "./tela-inicial.component.css",
 })
 export class TelaInicialComponent {
   cliente = {
-    nome: 'João da Silva',
-    numeroConta: '123456789',
+    nome: "João da Silva",
+    numeroConta: "123456789",
     saldo: 5000.5,
     transacoes: [
-      { data: new Date(), descricao: 'Depósito', valor: 1000.0 },
-      { data: new Date(), descricao: 'Compra', valor: -500.5 },
+      { data: new Date(), descricao: "Depósito", valor: 1000.0 },
+      { data: new Date(), descricao: "Compra", valor: -500.5 },
       // Adicione mais transações conforme necessário
     ],
   };
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TransactionComponent, {
+      width: "250px",
+      data: {
+        modalTitle: "Perform Transaction",
+        transactionAmount: null,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
